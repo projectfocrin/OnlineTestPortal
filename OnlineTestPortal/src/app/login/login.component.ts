11776921fbf3732278e649a8340794510a181c6f
projectfocrin <vishnugautam@projectfocrin.com>
2023-05-02 12:42:12 +0530

    loginpage and server is almost ready

--- OnlineTestPortal/src/app/login/login.component.ts
+++ OnlineTestPortal/src/app/login/login.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataserviceService } from '../dataservice.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public fullName!: string | null | undefined;
+  public password!: string | null | undefined;
+  user = { fullName: '', password: '' };
 
+  constructor(private ds: DataserviceService, private router: Router) {}
+  ngOnInit(): void {}
+
+  loginForm = new FormGroup({
+    fullName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+  });
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    console.table(this.loginForm.value);
+    this.fullName = this.loginForm.get('fullName')?.value;
+    this.password = this.loginForm.get('password')?.value;
+    // The error message Type 'string | null | undefined' is not assignable to type 'string'. Type 'undefined' is not assignable to type 'string'. is a TypeScript error that occurs when you try to assign a value of type string | null | undefined to a variable of type string1.
+    // In your code, the error is caused by the line this.user = {fullName: this.fullName, password: this.password};
+    //  The error message suggests that the value of this.fullName or this.password might be null or undefined1.
+    // This will only assign the value if both this.fullName and this.password are not null or undefined.
+    if (this.fullName && this.password) {
+      this.user = { fullName: this.fullName, password: this.password };
+    }
+    console.log(this.user);
+
+    this.ds.checkLogin(this.user).subscribe({
+      next: (data) => {
+        console.log(data);
+
+        if (data) {
+          alert('Login successful!');
+          sessionStorage.setItem('user', this.user.fullName);
+          this.router.navigate(['/quizes']);
+        }
+      },
+    });
+
+    //   if (this.fullName == 'vishnu' && this.password == '12345') {
+    //     this.router.navigate(['/quizes']);
+    //   } else {
+    //     console.log('error');
+    //   }
+    // }
+  }
 }
